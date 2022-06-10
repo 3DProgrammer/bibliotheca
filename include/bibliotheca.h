cbb61784dbb04d5671d13b951c69d886d141da0d
3DProgrammer <78192215+3DProgrammer@users.noreply.github.com>
2022-06-11 09:49:55 +1000

    Initial commit, no functionality yet.

---
+++ include/bibliotheca.h
@@ -0,0 +1,9 @@
+#ifndef BIBLIOTHECA_BIBLIOTHECA_H
+#define BIBLIOTHECA_BIBLIOTHECA_H
+
+namespace bibliotheca {
+    const int MAJOR_VERSION=0;
+    const int MINOR_VERSION=0;
+}
+
+#endif //BIBLIOTHECA_BIBLIOTHECA_H

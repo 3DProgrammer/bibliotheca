61aa4d865678d4ffc9739ee83a049e2039abec48
3DProgrammer <78192215+3DProgrammer@users.noreply.github.com>
2022-06-11 12:30:50 +1000

    Basic noun conjugating.

--- include/word.h
+++ include/word.h
@@ -12,6 +12,20 @@ namespace bibliotheca {
         f,
         n
     };
+    /*! Enum representing case of a noun. */
+    enum class Case {
+        nom,
+        acc,
+        gen,
+        dat,
+        abl,
+        voc
+    };
+    /*! Enum representing grammatical number. */
+    enum class Number {
+        singular,
+        plural
+    };
     template<size_t N>
     using PrincipalParts = std::array<std::string, N>;
     typedef std::vector<std::string> Translations;
@@ -29,11 +43,20 @@ namespace bibliotheca {
      * Class representing a noun.
      */
     class Noun : public Word {
+    private:
+        std::string stem;
     public:
         PrincipalParts<2> spelling; //!<The nominative singular and genitive singular form of the word.
         Translations english; //!<The singular English translations of the word.
         Gender gender; //!<The grammatical gender of the noun.
         int declension; //!<The declension of the noun.
+        /*!
+         * Declines the noun in the specified case and number.
+         * @param nounCase The grammatical case of the noun.
+         * @param number The grammatical number of the noun.
+         * @return The spelling of the noun.
+         */
+        std::string noun(Case nounCase, Number number);
     };
 }
 #endif //BIBLIOTHECA_WORD_H
--- word.cpp
+++ word.cpp
@@ -3,5 +3,22 @@
 //
 
 #include <bibliotheca.h>
+#include <map>
 #include "word.h"
+std::map<std::pair<bibliotheca::Case,bibliotheca::Number>,std::string> endings[5]={{
+    {{bibliotheca::Case::nom, bibliotheca::Number::singular},"us"},{{bibliotheca::Case::acc, bibliotheca::Number::singular},"um"},{{bibliotheca::Case::gen, bibliotheca::Number::singular},"i"},{{bibliotheca::Case::dat, bibliotheca::Number::singular},"o"},{{bibliotheca::Case::abl, bibliotheca::Number::singular},"o"},
+    {{bibliotheca::Case::nom, bibliotheca::Number::plural},"i"},{{bibliotheca::Case::acc, bibliotheca::Number::plural},"os"},{{bibliotheca::Case::gen, bibliotheca::Number::plural},"orum"},{{bibliotheca::Case::dat, bibliotheca::Number::plural},"is"},{{bibliotheca::Case::abl, bibliotheca::Number::plural},"is"}
+}};
 
+std::string bibliotheca::Noun::noun(bibliotheca::Case nounCase, bibliotheca::Number number) {
+    if (nounCase==Case::nom && number==Number::singular) {
+        return spelling[0];
+    }
+    if (gender==Gender::n&&number==Number::plural&&(nounCase==Case::nom||nounCase==Case::acc||nounCase==Case::voc)) {
+        return stem+"a";
+    }
+    if (nounCase==Case::voc) {
+        return noun(Case::nom, number); //TODO: Fix vocative case.
+    }
+    return stem+endings[declension][{nounCase,number}];
+}
